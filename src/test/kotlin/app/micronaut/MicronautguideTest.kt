80702ce7c919ba4b13114840c2ac3b793d5ed2f8
guivdh <guillaume-vdh@skynet.be>
2024-03-21 21:14:02 +0100

    Files name refactor

--- src/test/kotlin/app/micronaut/MicronautguideTest.kt
+++ src/test/kotlin/app/micronaut/MicronautguideTest.kt
@@ -6,7 +6,7 @@ import org.junit.jupiter.api.Test
 import jakarta.inject.Inject
 
 @MicronautTest
-class sensors-apiTest {
+class SensorsApiTest {
 
     @Inject
     lateinit var application: EmbeddedApplication<*>
